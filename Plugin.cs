18af39664dcf504d379d1eebfb7c26bf06557c78
jdjdmd <115753425+jdjdmd@users.noreply.github.com>
2024-08-01 12:59:26 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MenuTemplate.Menu;
+using MelonLoader;
+using Photon.Pun;
+using GorillaNetworking;
+using UnityEngine;
+
+namespace MenuTemplate
+{
+	public class Plugin : MelonMod
+	{
+        #region Main
+
+        public bool menuInitialized;
+
+		public override void OnUpdate()
+		{
+			if (!this.menuInitialized)
+			{
+				this.menuInitialized = true;
+				//if you want a mod to be enabled on load put this
+				//Menu.Menu.Page1ButtonActive[1] = true;
+			}
+
+			Menu.Menu.Prefix();
+			if (!PhotonNetwork.IsConnected)
+			{
+				PhotonNetwork.ConnectUsingSettings();
+			}
+		}
+
+		#endregion
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Johnpork
+# ModMenuTemplate
+## Made By Zinx, Edited By LMB Yeetus, And Also Credits To Maximility For His Template
